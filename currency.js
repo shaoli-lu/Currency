526378e15933e832d8cc7b8519c494947f8a7053
shaoli-lu <shaoli_lu@yahoo.com>
2022-09-01 22:27:20 -0500

    added comma

--- currency.js
+++ currency.js
@@ -6,33 +6,35 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#date').innerHTML = "As of: " + data.date;    
     
     rates = data.rates;
-    document.querySelector('#cny').innerHTML = 'Chinese Yuan: ' + rates.CNY.toFixed(2);  
-    document.querySelector('#vnd').innerHTML = 'Vietnam Dong: ' + rates.VND.toFixed(2);      
-    document.querySelector('#thb').innerHTML = 'Thai Baht: ' + rates.THB.toFixed(2);    
-    document.querySelector('#krw').innerHTML = 'Korean Won:   ' + rates.KRW.toFixed(2); 
-    document.querySelector('#jpy').innerHTML = 'Japanese Yen:   ' + rates.JPY.toFixed(2); 
-    document.querySelector('#inr').innerHTML = 'Indian Rupee:   ' + rates.INR.toFixed(2); 
-    document.querySelector('#aud').innerHTML = 'Australian Dollar:   ' + rates.AUD.toFixed(2); 
-    document.querySelector('#gbp').innerHTML = 'British Pound:   ' + rates.GBP.toFixed(2); 
-    document.querySelector('#rub').innerHTML = 'Russian Ruble:   ' + rates.RUB.toFixed(2); 
-    document.querySelector('#cad').innerHTML = 'Canadian Dollar:   ' + rates.CAD.toFixed(2); 
-    document.querySelector('#mxn').innerHTML = 'Mexican Peso:   ' + rates.MXN.toFixed(2); 
-    document.querySelector('#sgd').innerHTML = 'Singapore $:   ' + rates.SGD.toFixed(2); 
-    document.querySelector('#eur').innerHTML = 'Euro:   ' + rates.EUR.toFixed(2); 
-    document.querySelector('#try').innerHTML = 'Turkish Lira: ' + rates.TRY.toFixed(2); 
-    document.querySelector('#php').innerHTML = 'Philippine Peso:   ' + rates.PHP.toFixed(2); 
-    document.querySelector('#myr').innerHTML = 'Malaysian Ringgit:   ' + rates.MYR.toFixed(2); 
-    document.querySelector('#cup').innerHTML = 'Cuban Peso:   ' + rates.CUP.toFixed(2); 
-    document.querySelector('#uah').innerHTML = 'Ukraine Hryvnia:   ' + rates.CUP.toFixed(2); 
-    document.querySelector('#pln').innerHTML = 'Polish Zloty:   ' + rates.PLN.toFixed(2); 
-    document.querySelector('#czk').innerHTML = 'Czech Koruna:   ' + rates.CZK.toFixed(2); 
-    document.querySelector('#all').innerHTML = 'Albanian Lek:   ' + rates.ALL.toFixed(2); 
-    document.querySelector('#dzd').innerHTML = 'Algerian Dinar:   ' + rates.DZD.toFixed(2); 
-    document.querySelector('#bgn').innerHTML = 'Bulgarian Lev:   ' + rates.BGN.toFixed(2); 
-    document.querySelector('#lak').innerHTML = 'Laotian Kip:   ' + rates.LAK.toFixed(2); 
-    document.querySelector('#khr').innerHTML = 'Cambodian Riel:   ' + rates.KHR.toFixed(2);
-    document.querySelector('#bdt').innerHTML = 'Bangladeshi Taka:   ' + rates.BDT.toFixed(2);  
-    
+	document.querySelector('#cny').innerHTML = 'Chinese Yuan: ' + rates.CNY.toLocaleString('en-US',  {minimumFractionDigits:2} );  
+    document.querySelector('#vnd').innerHTML = 'Vietnam Dong: ' + rates.VND.toLocaleString('en-US',  {minimumFractionDigits:2} );      
+    document.querySelector('#thb').innerHTML = 'Thai Baht: ' + rates.THB.toLocaleString('en-US',  {minimumFractionDigits:2} );    
+    document.querySelector('#krw').innerHTML = 'Korean Won:   ' + rates.KRW.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#jpy').innerHTML = 'Japanese Yen:   ' + rates.JPY.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#inr').innerHTML = 'Indian Rupee:   ' + rates.INR.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#aud').innerHTML = 'Australian Dollar:   ' + rates.AUD.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#gbp').innerHTML = 'British Pound:   ' + rates.GBP.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#rub').innerHTML = 'Russian Ruble:   ' + rates.RUB.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#cad').innerHTML = 'Canadian Dollar:   ' + rates.CAD.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#mxn').innerHTML = 'Mexican Peso:   ' + rates.MXN.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#sgd').innerHTML = 'Singapore $:   ' + rates.SGD.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#eur').innerHTML = 'Euro:   ' + rates.EUR.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#try').innerHTML = 'Turkish Lira: ' + rates.TRY.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#php').innerHTML = 'Philippine Peso:   ' + rates.PHP.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#myr').innerHTML = 'Malaysian Ringgit:   ' + rates.MYR.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#cup').innerHTML = 'Cuban Peso:   ' + rates.CUP.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#uah').innerHTML = 'Ukraine Hryvnia:   ' + rates.CUP.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#pln').innerHTML = 'Polish Zloty:   ' + rates.PLN.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#czk').innerHTML = 'Czech Koruna:   ' + rates.CZK.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#all').innerHTML = 'Albanian Lek:   ' + rates.ALL.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#dzd').innerHTML = 'Algerian Dinar:   ' + rates.DZD.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#bgn').innerHTML = 'Bulgarian Lev:   ' + rates.BGN.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#lak').innerHTML = 'Laotian Kip:   ' + rates.LAK.toLocaleString('en-US',  {minimumFractionDigits:2} ); 
+    document.querySelector('#khr').innerHTML = 'Cambodian Riel:   ' + rates.KHR.toLocaleString('en-US',  {minimumFractionDigits:2} );
+    document.querySelector('#bdt').innerHTML = 'Bangladeshi Taka:   ' + rates.BDT.toLocaleString('en-US',  {minimumFractionDigits:2} );  
+    document.querySelector('#pkr').innerHTML = 'Pakistan Rupee:   ' + rates.PKR.toLocaleString('en-US',  {minimumFractionDigits:2} );  
+    document.querySelector('#brl').innerHTML = 'Brazilian Real:   ' + rates.BRL.toLocaleString('en-US',  {minimumFractionDigits:2} );  
+
  })
 
 })
